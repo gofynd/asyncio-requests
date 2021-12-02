cd9613ccee44913cc3c85a086647ba0bbff76dd5
Arjunsingh Yadav <arjunsinghyadav@gofynd.com>
2021-12-02 14:36:26 +0530

    temp changes

--- aio_requests/aio_request.py
+++ aio_requests/aio_request.py
@@ -7,9 +7,11 @@ async def request(
         data,
         auth=None,
         protocol="",
-        protocol_info=None
+        protocol_info=None,
+        pre_processor=None,
+        post_processor=None
 ):
-    common_response = {
+    response = {
         "url": url,
         "payload": data,
         "external_call_request_time": str(get_ist_now()),
@@ -18,8 +20,16 @@ async def request(
     }
 
     if not protocol_mapping.get(protocol.upper()):
-        common_response["error_message"] = "No Protocol Specified"
-        return common_response
+        response["error_message"] = "No Protocol Specified"
+        return response
 
-    response = await protocol_mapping[protocol](url, data, auth, common_response, info=protocol_info)
+    if pre_processor:
+        response["pre_processor_response"] = await pre_processor(url, data, auth, protocol, response, info=protocol_info)
+
+    processor_response = await protocol_mapping[protocol](url, data, auth, response, info=protocol_info)
+    response.update(processor_response)
+
+    if post_processor:
+        response["post_processor_response"] = await post_processor(url, data, auth, response, info=protocol_info)
+
     return response
