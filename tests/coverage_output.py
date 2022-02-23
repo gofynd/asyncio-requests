db372e959288e776e1b46b81c25488ef940d8994
Manish Magnani <manish.magnani@marutitech.com>
2022-02-23 10:26:46 +0530

    pytest + flake8

--- tests/coverage_output.py
+++ tests/coverage_output.py
@@ -3,17 +3,17 @@
 import json
 import xml.etree.ElementTree as ET
 
-tree = ET.parse("coverage/coverage.xml")
+tree = ET.parse('coverage/coverage.xml')
 attrib = tree.getroot().attrib
 
 out = {
-    "coverage_pct": float(attrib.get("line-rate")) * 100,
-    "lines_total": int(attrib.get("lines-valid")),
-    "lines_covered": int(attrib.get("lines-covered")),
-    "branch_pct": int(attrib.get("branch-rate")),
-    "branches_covered": int(attrib.get("branches-covered")),
-    "branches_total": int(attrib.get("branches-valid")),
+    'coverage_pct': float(attrib.get('line-rate')) * 100,
+    'lines_total': int(attrib.get('lines-valid')),
+    'lines_covered': int(attrib.get('lines-covered')),
+    'branch_pct': int(attrib.get('branch-rate')),
+    'branches_covered': int(attrib.get('branches-covered')),
+    'branches_total': int(attrib.get('branches-valid')),
 }
 
-with open("coverage/coverage_output.json", 'w') as outfile:
+with open('coverage/coverage_output.json', 'w') as outfile:
     json.dump(out, outfile)
