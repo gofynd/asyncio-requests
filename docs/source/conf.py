c968166df1ffd3641d7e99438cce9199387914df
Arjunsingh <arjunsinghyadav@gofynd.com>
2022-07-09 12:07:11 +0530

    changed requirements, maintainers

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2022, Fynd'  # noqa A001
 author = 'Arjunsingh Yadav'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0'
+release = '2.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -12,27 +12,27 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='asyncio-requests',
-    version='2.0',
-    author='Arjunsingh Yadav, Manish Magnani',
+    version='2.1',
+    author='Arjunsingh Yadav, Manish Magnani, Devesh Ratthour',
     author_email='arjunsinghyadav@fynd.com, manishmagnani@gofynd.com',
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/gofynd/asyncio-requests',
-    download_url='https://github.com/gofynd/aio-requests/archive/refs/tags/v2.0.tar.gz',  # noqa E251
+    download_url='https://github.com/gofynd/aio-requests/archive/refs/tags/v2.1.tar.gz',  # noqa E251
     packages=find_packages(
         exclude=('local_development', 'tests*', 'docs')),
     license='MIT',
     install_requires=[
         'aiohttp==3.8.1',
-        'ujson==5.1.0',
-        'requests==2.27.1',
+        'ujson==5.4.0',
+        'requests==2.28.1',
         'zeep[async]==4.0.0',
-        'aioboto3==9.3.1',
+        'aioboto3==9.6.0',
         'aiofiles==0.8.0',
         'pyfailsafe==0.6.0',
-        'pytz==2021.3',
-        'aioftp==0.20.1'
+        'pytz==2022.1',
+        'aioftp==0.21.2'
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
